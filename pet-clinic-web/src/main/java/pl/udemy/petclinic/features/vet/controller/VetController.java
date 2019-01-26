6b124cabe71e84c56bcc49b15ad1ea83c3a2b770
Dawid Janik <daw_janik@protonmail.com>
2019-01-26 20:11:56 +0100

    Added Vet list and updated controller. Closes #23

--- pet-clinic-web/src/main/java/pl/udemy/petclinic/features/vet/controller/VetController.java
+++ pet-clinic-web/src/main/java/pl/udemy/petclinic/features/vet/controller/VetController.java
@@ -1,15 +1,24 @@
 package pl.udemy.petclinic.features.vet.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.udemy.petclinic.vet.VetService;
 
 @Controller
 @RequestMapping("vets")
 @SuppressWarnings("unused")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listsVets() {
+    public String listsVets(Model model) {
+        model.addAttribute("vets", vetService.getAll());
         return "vets/index";
     }
 }
