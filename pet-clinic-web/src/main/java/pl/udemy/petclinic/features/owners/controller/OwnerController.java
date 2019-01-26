d6c030915b0fe9e8ad039f066957c2fd277c467e
Dawid Janik <daw_janik@protonmail.com>
2019-01-26 19:57:46 +0100

    Impl Owner list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/pl/udemy/petclinic/features/owners/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/udemy/petclinic/features/owners/controller/OwnerController.java
@@ -1,15 +1,24 @@
 package pl.udemy.petclinic.features.owners.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.udemy.petclinic.owner.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 @SuppressWarnings("unused")
 public class OwnerController {
 
-    @RequestMapping({ "","/index", "/owners/index.html"})
-    public String listOwners() {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/index", "/owners/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.getAll());
         return "owners/index";
     }
 }
