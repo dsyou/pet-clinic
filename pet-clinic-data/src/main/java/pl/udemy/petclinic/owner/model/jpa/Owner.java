552496b56470a4fb43a5ce9f27bfbad460ade071
Dawid Janik <daw_janik@protonmail.com>
2019-03-02 19:54:05 +0100

    Added properties. Closes #29.

--- pet-clinic-data/src/main/java/pl/udemy/petclinic/owner/model/jpa/Owner.java
+++ pet-clinic-data/src/main/java/pl/udemy/petclinic/owner/model/jpa/Owner.java
@@ -10,8 +10,36 @@ import java.util.Set;
  * @since 01.12.2018
  */
 public class Owner extends Person {
+
+    private String city;
+    private String address;
+    private String telephone;
+
     private Set<Pet> pets;
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
