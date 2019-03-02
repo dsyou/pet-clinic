03a55dcbff03ca903f427f1c31be89ff35f9754b
Dawid Janik <daw_janik@protonmail.com>
2019-03-02 20:14:00 +0100

    Added PetTypeSerice. Closes #30.

---
+++ pet-clinic-data/src/main/java/pl/udemy/petclinic/pet/PetTypeService.java
@@ -0,0 +1,11 @@
+package pl.udemy.petclinic.pet;
+
+import pl.udemy.petclinic.core.CrudService;
+import pl.udemy.petclinic.pet.model.jpa.PetType;
+
+/**
+ *
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
--- pet-clinic-data/src/main/java/pl/udemy/petclinic/pet/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/pl/udemy/petclinic/pet/PetTypeServiceMap.java
@@ -3,6 +3,7 @@ package pl.udemy.petclinic.pet;
 import org.springframework.stereotype.Service;
 import pl.udemy.petclinic.core.AbstractMapService;
 import pl.udemy.petclinic.pet.model.jpa.Pet;
+import pl.udemy.petclinic.pet.model.jpa.PetType;
 
 import java.util.Set;
 
@@ -11,30 +12,26 @@ import java.util.Set;
  * @since 01.12.2018
  */
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
 
     @Override
-    public Set<Pet> getAll() {
+    public Set<PetType> getAll() {
         return super.getAll();
     }
 
     @Override
-    public Pet getById(Long id) {
-        return super.getById(id);
+    public PetType create(PetType petType) {
+        return super.create(petType);
     }
 
     @Override
-    public Pet create(Pet pet) {
-        return super.create(pet);
-    }
-
-    @Override
-    public void delete(Pet pet) {
-        super.delete(pet);
+    public void delete(PetType petType) {
+        super.delete(petType);
     }
 
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
     }
+
 }
