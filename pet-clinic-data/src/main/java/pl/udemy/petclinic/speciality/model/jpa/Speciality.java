92de048a4e47e3416d40340593c6dda9ff1dadb2
Dawid Janik <daw_janik@protonmail.com>
2019-03-05 20:23:37 +0100

    Updated Vet to use JPA. Closes #38.

--- pet-clinic-data/src/main/java/pl/udemy/petclinic/speciality/model/jpa/Speciality.java
+++ pet-clinic-data/src/main/java/pl/udemy/petclinic/speciality/model/jpa/Speciality.java
@@ -2,12 +2,19 @@ package pl.udemy.petclinic.speciality.model.jpa;
 
 import pl.udemy.petclinic.core.model.jpa.BaseEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author Dawid Janik
  * @since 02.03.2019
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/pl/udemy/petclinic/vet/model/jpa/Vet.java
+++ pet-clinic-data/src/main/java/pl/udemy/petclinic/vet/model/jpa/Vet.java
@@ -3,6 +3,7 @@ package pl.udemy.petclinic.vet.model.jpa;
 import pl.udemy.petclinic.core.model.jpa.person.Person;
 import pl.udemy.petclinic.speciality.model.jpa.Speciality;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,8 +11,13 @@ import java.util.Set;
  * @author Dawid Janik
  * @since 01.12.2018
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
