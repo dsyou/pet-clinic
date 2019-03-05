f3f0752f305174d6c356a18147c4205a00fe0a30
Dawid Janik <daw_janik@protonmail.com>
2019-03-05 20:32:42 +0100

    Added Visit entity. Closes #29.

--- pet-clinic-data/src/main/java/pl/udemy/petclinic/pet/model/jpa/Pet.java
+++ pet-clinic-data/src/main/java/pl/udemy/petclinic/pet/model/jpa/Pet.java
@@ -1,10 +1,14 @@
 package pl.udemy.petclinic.pet.model.jpa;
 
+import ch.qos.logback.classic.ViewStatusMessagesServlet;
 import pl.udemy.petclinic.core.model.jpa.BaseEntity;
 import pl.udemy.petclinic.owner.model.jpa.Owner;
+import pl.udemy.petclinic.visit.model.jpa.Visit;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Dawid Janik
@@ -27,6 +31,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -58,5 +65,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
--- pet-clinic-data/src/main/java/pl/udemy/petclinic/visit/model/jpa/Visit.java
+++ pet-clinic-data/src/main/java/pl/udemy/petclinic/visit/model/jpa/Visit.java
@@ -1,18 +1,27 @@
-package pl.udemy.petclinic.visit;
+package pl.udemy.petclinic.visit.model.jpa;
 
 import pl.udemy.petclinic.core.model.jpa.BaseEntity;
 import pl.udemy.petclinic.pet.model.jpa.Pet;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * @author Dawid Janik
  * @since 27.02.2019
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
