4a1f894e4f35e46b1fe4f4ed2d011e577df98325
Dawid Janik <daw_janik@protonmail.com>
2019-04-15 20:47:04 +0200

    Impl MockMvc test for Owner Controller. Closes #52.

--- pet-clinic-data/src/test/java/pl/udemy/petclinic/owner/OwnerJpaServiceShould.java
+++ pet-clinic-data/src/test/java/pl/udemy/petclinic/owner/OwnerJpaServiceShould.java
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class OwnerJpaServiceTest {
+public class OwnerJpaServiceShould {
 
     public static final String LAST_NAME = "Smith";
 
